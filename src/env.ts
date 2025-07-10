18716129403269439ac86fcf149fdb30c2a73d9a
Matheus Zarpellon <mvzarpellon@gmail.com>
2025-07-09 22:42:49 -0300

    aula 3

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,8 @@ import { z } from 'zod';
 
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
-  DATABASE_URL: z.string().url().startsWith('postgres://')
+  DATABASE_URL: z.string().url().startsWith('postgres://'),
+  GEMINI_API_KEY: z.string(),
 });
 
 export const env = envSchema.parse(process.env);
